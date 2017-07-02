9b699b50cdd0078de5a925754e9f734909ac3248
Amit Rahav <amit.r.89@gmail.com>
2017-07-02 09:52:28 +0300

    invoking codepipeline

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 'use strict';
 
+const SlackWebhook = require('slack-webhook')
+
 exports.handler = (event, context, callback) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
-    const SlackWebhook = require('slack-webhook')
+    console.log('Received event:', JSON.stringify(event, null, 2));
  
+		// ----- SOME CODEPIPELINE EVENT OBJECTS ---- //
+	  let codepipeline = new AWS.CodePipeline();
+	  // Retrieve the Job ID from the Lambda action
+    const CPjobId = event["CodePipeline.job"].id;
+    
+    // Retrieve the value of UserParameters from the Lambda action configuration in AWS CodePipeline, in this case a URL which will be
+    // health checked by this function.
+    const CPurl = event["CodePipeline.job"].data.actionConfiguration.configuration.UserParameters; 
+
+    
+		// ----- GENRAL VARS ---- //
 		let channel = process.env.CHANNEL || '#general'
 		let SlackWebhookUrl = process.env.SLACKWEBHOOKURL || 'https://hooks.slack.com/services/user/hook'
-		let deployUrl = process.env.URL || 'http://ec2-someIP.compute.amazonaws.com/';
+		let deployUrl = CPurl || process.env.URL || 'http://ec2-someIP.compute.amazonaws.com/';
 		let message = process.env.MESSAGE || 'Successfully deploy at ';
 
 
+    var putJobSuccess = function(message) {
+        var params = {
+            jobId: jobId
+        };
+        codepipeline.putJobSuccessResult(params, function(err, data) {
+            if(err) {
+                context.fail(err);      
+            } else {
+                context.succeed('all went fine');      
+            }
+        });
+    };
+
+    var putJobFailure = function(message) {
+        var params = {
+            jobId: jobId,
+            failureDetails: {
+                message: JSON.stringify(message),
+                type: 'JobFailed',
+                externalExecutionId: context.invokeid
+            }
+        };
+        codepipeline.putJobFailureResult(params, function(err, data) {
+            context.fail(message);      
+        });
+    };
+
 		let slack = new SlackWebhook(SlackWebhookUrl);
-
-
 		slack.send({
 		  text: message + deployUrl,
 		  username: 'AWS finshed auto code deploy',
@@ -20,10 +57,11 @@ exports.handler = (event, context, callback) => {
 		  channel: channel
 		}).then(function (res) {
 		  // succesful request 
-    	callback(null, 'all went fine');  // Echo back the first key value
+		  // Notify AWS CodePipeline of a successful job
+    	putJobSuccess('all went fine');  // Echo back the first key value
 		}).catch(function (err) {
 		  // handle request error 
-    	callback('Something went wrong', err);  // Echo back the first key value
+    	putJobFailure(err);  // Echo back the first key value
 		});
 
 };
