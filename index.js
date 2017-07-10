bd0e8b89264e9a206bcf526c20be36583a33901b
Amit Rahav <amit.r.89@gmail.com>
2017-07-10 14:34:32 +0300

    add userparmeters into read me and code

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ const AWS = require('aws-sdk');
 exports.handler = (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
  
-		// ----- SOME CODEPIPELINE EVENT OBJECTS ---- //
-	  let codepipeline = new AWS.CodePipeline();
-	  // Retrieve the Job ID from the Lambda action
+	// ----- SOME CODEPIPELINE EVENT OBJECTS ---- //
+    let codepipeline = new AWS.CodePipeline();
+    // Retrieve the Job ID from the Lambda action
     const CPjobId = event["CodePipeline.job"].id;
     
     // Retrieve the value of UserParameters from the Lambda action configuration in AWS CodePipeline, in this case a URL which will be
     // health checked by this function.
-    const CPurl = event["CodePipeline.job"].data.actionConfiguration.configuration.UserParameters; 
+    const CPuserParameters = event["CodePipeline.job"].data.actionConfiguration.configuration.UserParameters; 
 
     
-		// ----- GENRAL VARS ---- //
-		let channel = process.env.CHANNEL || '#general'
-		let SlackWebhookUrl = process.env.SLACKWEBHOOKURL || 'https://hooks.slack.com/services/user/hook'
-		let deployUrl = CPurl || process.env.URL || 'http://ec2-someIP.compute.amazonaws.com/';
-		let message = process.env.MESSAGE || 'Successfully deploy at ';
+	// ----- GENRAL VARS ---- //
+	let channel = CPuserParameters.channel || process.env.CHANNEL || '#general'
+	let SlackWebhookUrl = CPuserParameters.slackwebhook || process.env.SLACKWEBHOOKURL || 'https://hooks.slack.com/services/user/hook'
+	let deployUrl = CPuserParameters.url || process.env.URL || 'http://ec2-someIP.compute.amazonaws.com/';
+	let message = CPuserParameters.message || process.env.MESSAGE || 'Successfully deploy at ';
 
 
     var putJobSuccess = function(message) {
@@ -51,19 +51,19 @@ exports.handler = (event, context, callback) => {
         });
     };
 
-		let slack = new SlackWebhook(SlackWebhookUrl);
-		slack.send({
-		  text: message + deployUrl,
-		  username: 'AWS finshed auto code deploy',
-		  icon_emoji: ':scream_cat:',
-		  channel: channel
-		}).then(function (res) {
-		  // succesful request 
-		  // Notify AWS CodePipeline of a successful job
-    	putJobSuccess('all went fine');  // Echo back the first key value
-		}).catch(function (err) {
-		  // handle request error 
-    	putJobFailure(err);  // Echo back the first key value
-		});
+	let slack = new SlackWebhook(SlackWebhookUrl);
+	slack.send({
+	  text: message + deployUrl,
+	  username: 'AWS finshed auto code deploy',
+	  icon_emoji: ':scream_cat:',
+	  channel: channel
+	}).then(function (res) {
+	  // succesful request 
+	  // Notify AWS CodePipeline of a successful job
+	   putJobSuccess('all went fine');  // Echo back the first key value
+	}).catch(function (err) {
+	  // handle request error 
+	   putJobFailure(err);  // Echo back the first key value
+	});
 
 };
--- readme.md
+++ readme.md
@@ -2,8 +2,15 @@
 this is a lambda function for sending slack message at any chanel.
 
 ##usage
-* upload zipfile.zip into aws new function.
-* use env veribals to set details:
+* upload zipfile.zip into aws new lambda function.
+* you can use aither codepipline parameters or lambda env vars.
+* codepipline user paramters usage:
+  * edit satge at code deploy. under aws lambda function name, input at the user paremetrs input this kind of json:
+  `{ "message" : "some slack message", "url" : "the webapp endpoint url",
+  "channel" : "#slack channel"
+  "slackwebhook" : "the webhook ul"
+   }` 
+* lambda env veribals to set details:
 	* MESSAGE = slack message
 	* URL = the link to attach at the end of the message.
 	* CHANNEL = #slack channel
